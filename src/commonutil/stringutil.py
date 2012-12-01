54c4ce2047ec28cd4e3fb7aa3c283cde13fa9ddf
Writing Lin <writing.lin@gmail.com>
2012-12-01 22:44:55 +0800

    Add stringutil.calculateHash

---
+++ src/commonutil/stringutil.py
@@ -0,0 +1,14 @@
+
+from md5 import md5
+
+def calculateHash(values):
+    lines = []
+    for value in values:
+        if not value:
+            continue
+        if type(value) == unicode:
+            lines.append(value.encode('utf-8','ignore'))
+        else:
+            lines.append(value)
+    return md5(''.join(lines)).hexdigest()
+
