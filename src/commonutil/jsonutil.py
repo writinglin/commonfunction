88b0f9704cfa417c3492dfa35a9995fced63e6de
Writing Lin <writing.lin@gmail.com>
2013-01-01 17:52:14 +0800

    Improve config manager to support big data
    GAE only support data < 1 M for db and memcache

--- src/commonutil/jsonutil.py
+++ src/commonutil/jsonutil.py
@@ -1,5 +1,15 @@
 import json
 
-def getReadableString(jsonobj):
-    return json.dumps(jsonobj, ensure_ascii=False, indent=4, sort_keys=True)
+import jsonpickle
+
+def getReadableString(jsonvalue):
+    try:
+        # json.dumps only supports basical data type,
+        # it doesn't support datetime.datetime
+        result = json.dumps(jsonvalue, ensure_ascii=False, indent=4, sort_keys=True)
+    except TypeError:
+        # use jsonpickle to dumps datetime, and use json.dumps to get control format
+        jsonvalue = json.loads(jsonpickle.encode(jsonvalue))
+        result = json.dumps(jsonvalue, ensure_ascii=False, indent=4, sort_keys=True)
+    return result
 
--- src/configmanager/cmapi.py
+++ src/configmanager/cmapi.py
@@ -1,7 +1,8 @@
 """
 Save item value to memcache to improve performance.
+
+Don't take concurrency into condideration. Please take care.
 """
-import json
 import logging
 from google.appengine.api import memcache
 from google.appengine.ext import db
@@ -11,61 +12,186 @@ import jsonpickle
 from commonutil import jsonutil
 from . import models
 
-class ConfigManager(object):
+MAX_ITEM_SIZE = 1024 * 900
+PART_KEY_SUFFIX = '.pend'
+
+"""
+A class support a config -> an entity in db -> a record in memcache.
+
+"""
+class BasicManager(object):
 
     def __init__(self, modelclass):
         self.modelclass = modelclass
 
-    def getRawItems(self):
-        items = []
-        for item in self.modelclass.all():
-            jsonobj = json.loads(item.value)
-            fstr = jsonutil.getReadableString(jsonobj)
-            items.append({'key': item.key().name(), 'value': fstr,})
-        return items
-
     def _getCacheKey(self, keyname):
         return '%s-%s' % (self.modelclass.kind(), keyname)
 
-    # thread safe visit to cache
-    def _setCache(self, cachekey, jsonvalue):
-        client = memcache.Client()
-        oldvalue = client.gets(cachekey)
-        success = False
-        if not oldvalue:
-            memcache.set(cachekey, jsonvalue)
-            success = True
+    def _getDbKey(self, keyname):
+        return keyname
+
+    def getItemValue(self, keyname=None, cachekey=None, dbkey=None,
+                        defaultValue=None, jsonType=True):
+        if keyname:
+            cachekey = self._getCacheKey(keyname)
+            dbkey = self._getDbKey(keyname)
+        cachevalue = memcache.get(cachekey)
+        if cachevalue is not None:
+            return cachevalue
+        configitem = self.modelclass.get_by_key_name(dbkey)
+        if configitem:
+            if jsonType:
+                cachevalue = jsonpickle.decode(configitem.value)
+            else:
+                cachevalue = configitem.value
+        if cachevalue is None:
+            cachevalue = defaultValue
+        memcache.set(cachekey, cachevalue)
+        return cachevalue
+
+    def removeItem(self, keyname=None, cachekey=None, dbkey=None):
+        if keyname:
+            cachekey = self._getCacheKey(keyname)
+            dbkey = self._getDbKey(keyname)
+        memcache.delete(cachekey)
+        keyobj = db.Key.from_path(self.modelclass.kind(), dbkey)
+        db.delete(keyobj)
+        return True
+
+    def saveItem(self, keyname=None, cachekey=None, dbkey=None,
+            jsonvalue=None, strvalue=None):
+        if keyname:
+            cachekey = self._getCacheKey(keyname)
+            dbkey = self._getDbKey(keyname)
+        if jsonvalue is None:
+            cachevalue = strvalue
+            dbvalue = strvalue
         else:
-            if client.cas(cachekey, jsonvalue):
-                success = True
-        return success
+            cachevalue = jsonvalue
+            dbvalue = jsonpickle.encode(jsonvalue)
+        memcache.set(cachekey, cachevalue)
+        item = self.modelclass(key_name=dbkey, value=dbvalue)
+        item.put()
+        return True
+
+
+"""
+A class supports a config storing as multi parts.
+"""
+class ConfigManager(BasicManager):
+
+    def __init__(self, modelclass):
+        super(ConfigManager, self).__init__(modelclass)
+
+    def getRawItems(self):
+        items = []
+        for item in self.modelclass.all():
+            dbkey = item.key().name()
+            if dbkey.endswith(PART_KEY_SUFFIX):
+                continue
+            value = self.getItemValue(dbkey)
+            fstr = jsonutil.getReadableString(value)
+            items.append({'key': dbkey, 'value': fstr,})
+        return items
+
+    def _getCacheKey(self, keyname, part=-1):
+        cachekey = super(ConfigManager, self)._getCacheKey(keyname)
+        if part < 0:
+            return cachekey
+        return '%s.%s%s' % (cachekey, part, PART_KEY_SUFFIX)
+
+    def _getDbKey(self, keyname, part=-1):
+        dbkey = super(ConfigManager, self)._getDbKey(keyname)
+        if part < 0:
+            return dbkey
+        return '%s.%s%s' % (dbkey, part, PART_KEY_SUFFIX)
+
+    def _getPartCount(self, jsonvalue):
+        return jsonvalue.get('partcount')
+
+    def _isBigItem(self, jsonvalue):
+        return type(jsonvalue) == dict and bool(self._getPartCount(jsonvalue))
+
+    def _isBigCount(self, partcount):
+        return partcount > 1
 
     def getItemValue(self, keyname, defaultValue=None):
-        cachekey = self._getCacheKey(keyname)
-        jsonvalue = memcache.get(cachekey)
-        if not jsonvalue:
-            configitem = self.modelclass.get_by_key_name(keyname)
-            if configitem:
-                jsonvalue = jsonpickle.decode(configitem.value)
-            else:
-                jsonvalue = defaultValue
-            self._setCache(cachekey, jsonvalue)
-        return jsonvalue
+        mainValue = super(ConfigManager, self).getItemValue(keyname)
+        if not self._isBigItem(mainValue):
+            return mainValue
+        partcount = self._getPartCount(mainValue)
+        partvalues = []
+        for i in range(partcount):
+            cachekey = self._getCacheKey(keyname, part=i)
+            dbkey = self._getCacheKey(keyname, part=i)
+            value = super(ConfigManager, self).getItemValue(
+                            cachekey=cachekey, dbkey=dbkey, jsonType=False)
+            partvalues.append(value)
+        mainstr = ''.join(partvalues)
+        return jsonpickle.decode(mainstr)
 
-    def saveItem(self, keyname, jsonvalue):
-        cachekey = self._getCacheKey(keyname)
-        success = self._setCache(cachekey, jsonvalue)
-        if success:# if we fail to save value to cache, we should not put it into db.
-            strvalue = jsonpickle.encode(jsonvalue)
-            item = self.modelclass(key_name=keyname, value=strvalue)
-            item.put()
-        return success
+    def _getPartCountByKey(self, keyname):
+        mainValue = super(ConfigManager, self).getItemValue(keyname)
+        if not mainValue:
+            return 0
+        if self._isBigItem(mainValue):
+            return self._getPartCount(mainValue)
+        return 1
+
+    def _removePartItem(self, keyname, part):
+        cachekey = self._getCacheKey(keyname, part=part)
+        dbkey = self._getDbKey(keyname, part=part)
+        super(ConfigManager, self).removeItem(
+                        cachekey=cachekey, dbkey=dbkey)
 
     def removeItem(self, keyname):
-        cachekey = self._getCacheKey(keyname)
-        memcache.delete(cachekey)
-        keyobj = db.Key.from_path(self.modelclass.kind(), keyname)
-        db.delete(keyobj)
+        partcount = self._getPartCountByKey(keyname)
+        super(ConfigManager, self).removeItem(keyname)
+        if not self._isBigCount(partcount):
+            return True
+        for i in range(partcount):
+            self._removePartItem(keyname, i)
+        return True
+
+    def _getPartCountByStr(self, strvalue):
+        valuelen = len(strvalue)
+        partcount = valuelen / MAX_ITEM_SIZE
+        if valuelen % MAX_ITEM_SIZE != 0:
+            partcount += 1
+        return partcount
+
+    def saveItem(self, keyname, jsonvalue):
+        oldPartCount = self._getPartCountByKey(keyname)
+
+        strvalue = jsonpickle.encode(jsonvalue)
+        newPartCount = self._getPartCountByStr(strvalue)
+
+        # remove the unreferenced part entities.
+        if self._isBigCount(oldPartCount) and newPartCount < oldPartCount:
+            if self._isBigCount(newPartCount):
+                deleteStart = newPartCount
+            else:
+                deleteStart = 0
+            for i in range(deleteStart, oldPartCount):
+                self._removePartItem(keyname, i)
+
+        if not self._isBigCount(newPartCount):
+            super(ConfigManager, self).saveItem(keyname, jsonvalue=jsonvalue)
+            return True
+
+        valuelen = len(strvalue)
+
+        mainJson = {'partcount': newPartCount, 'size': valuelen}
+        super(ConfigManager, self).saveItem(keyname, jsonvalue=mainJson)
+
+        for i in range(newPartCount):
+            cachekey = self._getCacheKey(keyname, i)
+            dbkey = self._getDbKey(keyname, i)
+            value = strvalue[MAX_ITEM_SIZE * i:
+                        min(valuelen, MAX_ITEM_SIZE * (i + 1))]
+            super(ConfigManager, self).saveItem(cachekey=cachekey,
+                                        dbkey=dbkey, strvalue=value)
+
         return True
 
 DEFAULT_MANAGER = None
