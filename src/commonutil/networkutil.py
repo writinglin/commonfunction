fcc25bde78e1bd762dd53348c12613c9e48a6783
Writing Lin <writing.lin@gmail.com>
2013-02-23 23:57:03 +0800

    Add newworkutil.isUuidHandled

--- src/commonutil/networkutil.py
+++ src/commonutil/networkutil.py
@@ -1,9 +1,15 @@
+import copy
 import json
 import logging
 import time
 import urllib2
+import uuid
+
+from configmanager import cmapi
 
 def postData(url, data, tag=None, trycount=1, timeout=10):
+    data = copy.deepcopy(data)
+    data['uuid'] = str(uuid.uuid4())
     success = False
     jsonData = json.dumps(data)
     for i in range(trycount):
@@ -20,4 +26,21 @@ def postData(url, data, tag=None, trycount=1, timeout=10):
             if leftcount > 0:
                 time.sleep(1)
     return success
+
+def isUuidHandled(uuid):
+    if not uuid:
+        return False
+    items = cmapi.getItemValue('~.uuids', [])
+    if uuid in items:
+        return True
+    return False
+
+def updateUuids(uuid):
+    _MAX_ITEM_COUNT = 100
+    if not uuid:
+        return
+    items = cmapi.getItemValue('~.uuids', [])
+    items.insert(0, uuid)
+    items = items[:_MAX_ITEM_COUNT]
+    cmapi.saveItem('~.uuids', items)
 
